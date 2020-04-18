a2b98384a4688a3ea6122684a84fce74085b58fc
Countra <1025276726@qq.com>
2020-04-18 17:26:58 +0800

    Update gdaria2.sh

--- drive/gdaria2.sh
+++ drive/gdaria2.sh
@@ -129,7 +129,7 @@ sudo systemctl daemon-reload
 echo -e "\033[41;33m 重启服务 \033[0m"
 service aria2c restart
 echo "已重启aria2c"
-systemctl restart filebrowser
-echo "filebrowser"
+service filebrowser restart
+echo "已重启filebrowser"
 echo -e "\033[41;33m Finish! \033[0m"
 echo "filebower地址: "http://$ip:8080" AriaNg: "http://$ip/lixian""
