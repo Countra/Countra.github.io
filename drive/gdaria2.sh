8abc383a9324fed26f1df0a0e3d1b50b61f6154c
Countra <1025276726@qq.com>
2020-04-18 16:42:36 +0800

    Update gdaria2.sh

--- drive/gdaria2.sh
+++ drive/gdaria2.sh
@@ -102,7 +102,7 @@ echo -e "\033[41;33m 完成安装fileBrowser \033[0m"
 
 
 
-echo "开始配置aria2"
+echo -e "开始配置aria2"
 sudo rm -rf /root/.aria2
 sudo mkdir -p /root/.aria2
 sudo rm -rf /root/.aria2/*
@@ -115,16 +115,16 @@ secret="rpc-secret=$p"
 upload="on-download-complete=/root/rcloneupload.sh"
 sudo echo $upload >> /root/.aria2/aria2.conf
 
-echo "\033[41;33m 设置systemctl \033[0m"
+echo -e "\033[41;33m 设置systemctl \033[0m"
 sudo cp $tmp/aria2c /etc/init.d/
 sudo cp $tmp/filebrowser /etc/init.d/
-echo "\033[41;33m 拷贝完成 \033[0m"
+echo -e "\033[41;33m 拷贝完成 \033[0m"
 sudo chmod 777  /etc/init.d/aria2c
 sudo chmod 777  /etc/init.d/filebrowser
-echo "\033[41;33m 授权完成 \033[0m"
+echo -e "\033[41;33m 授权完成 \033[0m"
 sudo systemctl daemon-reload
-echo "\033[41;33m 重启服务 \033[0m"
+echo -e "\033[41;33m 重启服务 \033[0m"
 sudo service aria2c restart
 sudo service filebrowser restart
-echo "\033[41;33m Finish! \033[0m"
+echo -e "\033[41;33m Finish! \033[0m"
 echo "filebower地址: "http://$ip:8080" AriaNg: "http://$ip/lixian""
