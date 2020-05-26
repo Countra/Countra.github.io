ca21f258892e5a0caacf76d08af0b4aa34e0301b
Countra <1025276726@qq.com>
2020-05-26 12:48:31 +0800

    add filebower's default admin

--- drive/gdaria2.sh
+++ drive/gdaria2.sh
@@ -133,5 +133,5 @@ service aria2c restart
 echo "已重启aria2c"
 echo "请手动重启filebrowser"
 echo -e "\033[41;33m Finish! \033[0m"
-echo "filebower地址: "http://$ip:8080" AriaNg: "http://$ip/lixian""
+echo "filebower地址: "http://$ip:8080"（默认账户密码均为admin） AriaNg: "http://$ip/lixian""
 echo "由于filebrowser不知道为什么就是重启脚本卡死，请手动重启 ：service filebrowser restart"
